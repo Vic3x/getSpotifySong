fc1d1d753a2fcae90f0886b4134f800d9daff112
Vincenzo <vincenzoguarnieri00@gmail.com>
2023-02-13 23:12:12 +0100

    Update spotify.js

--- spotify.js
+++ spotify.js
@@ -16,21 +16,36 @@ const options = {
 };
 
 
-const server = http.createServer((req, res) => {
+let currentSong = '';
 
-
-
+const checkForUpdates = () => {
     request(options)
         .then(function (response) {
-            if (response.is_playing) {
-                res.end('Current song: ' + response.item.name);
-            } else {
-                res.end('Nothing is playing');
+            if (response.is_playing && response.item.name !== currentSong) {
+                currentSong = response.item.name;
+                console.log(`Current song changed to: ${currentSong}`);
+                location.reload();
             }
         })
         .catch(function (err) {
             console.error(err);
-            res.end('An error occured');
+        });
+};
+
+
+const server = http.createServer((req, res) => {
+    request(options)
+        .then(function (response) {
+            if (response.is_playing) {
+                currentSong = response.item.name;
+                res.end(`<html><head><meta http-equiv="refresh" content="5"></head><body>Current song: ${currentSong}</body></html>`);
+            } else {
+                res.end('<html><head><meta http-equiv="refresh" content="5"></head><body>Nothing is playing</body></html>');
+            }
+        })
+        .catch(function (err) {
+            console.error(err);
+            res.end('<html><head><meta http-equiv="refresh" content="5"></head><body>An error occurred</body></html>');
         });
 });
 
